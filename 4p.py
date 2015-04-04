35bbe29f316e4a4a83f75569d95d33afed70cd25
Peter Román <peterrom@gmail.com>
2015-04-04 10:51:15 +0200

    Add initial prototype.

---
+++ 4p.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# Copyright 2015 Peter Román
+# This file is part of 4p which is licensed under GNU GPL v3.
+# See the file named LICENSE for details.
+
+# This file contains extremely crude prototype code.
+
+import sys
+import re
+import subprocess
+
+def parse(s):
+    m = re.search('(\$+\()', s)
+
+    if not m:
+        return [ s ]
+
+    a = s[:m.start()]
+    b, c = s[m.end():].split(')' + m.group().strip('('), 1)
+
+    return [ a, b ] + parse(c)
+
+
+ppfile = sys.argv[1]
+assert ppfile.endswith('.4p')
+
+s = open(ppfile).read()
+f = open(ppfile[:-3], mode='w')
+
+p = parse(s) + ['']
+
+for a, b in zip(p[0::2], p[1::2]):
+    f.write(a + subprocess.check_output([ '/bin/bash',
+                                          '-c',
+                                          b ])
+            .decode('utf-8')
+            .strip('\n'))
