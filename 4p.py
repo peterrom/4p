48f2d3188b526be6847d7bd4402b0add907a4bd9
Peter Román <peterrom@gmail.com>
2016-03-28 10:13:56 +0200

    Less retarded formatting, please.

--- 4p.py
+++ 4p.py
@@ -6,33 +6,37 @@
 
 # This file contains extremely crude prototype code.
 
-import sys
 import re
 import subprocess
+
 
 def parse(s):
     m = re.search('(\$+\()', s)
 
     if not m:
-        return [ s ]
+        return [s]
 
     a = s[:m.start()]
     b, c = s[m.end():].split(')' + m.group().strip('('), 1)
 
-    return [ a, b ] + parse(c)
+    return [a, b] + parse(c)
 
 
-ppfile = sys.argv[1]
-assert ppfile.endswith('.4p')
+def main(ppfile):
+    assert ppfile.endswith('.4p')
 
-s = open(ppfile).read()
-f = open(ppfile[:-3], mode='w')
+    s = open(ppfile).read()
+    f = open(ppfile[:-3], mode='w')
 
-p = parse(s) + ['']
+    p = parse(s) + ['']
 
-for a, b in zip(p[0::2], p[1::2]):
-    f.write(a + subprocess.check_output([ '/bin/bash',
-                                          '-c',
-                                          b ])
-            .decode('utf-8')
-            .strip('\n'))
+    for a, b in zip(p[0::2], p[1::2]):
+        f.write(a + subprocess.check_output(['/bin/bash',
+                                             '-c',
+                                             b])
+                .decode('utf-8')
+                .strip('\n'))
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1])
