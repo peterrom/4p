080549140c726ed022fa411b23a1f458b8bb303e
Peter Román <peterrom@gmail.com>
2016-04-05 20:07:17 +0200

    Spruce up test with hypothesis.

--- test4p.py
+++ test4p.py
@@ -1,11 +1,18 @@
 from subprocess import Popen, PIPE
 
+from hypothesis import given, assume
+from hypothesis.strategies import text
+
 
 def parse(data):
-    output, _ = Popen(['./4p'], stdin=PIPE, stdout=PIPE).communicate(data)
-    return output
+    output, _ = Popen(['./4p'], stdin=PIPE, stdout=PIPE) \
+        .communicate(data.encode('utf-8'))
+
+    return output.decode('utf-8')
 
 
-def test_no_cmd():
-    data = 'abcdefg'
+@given(text())
+def test_no_cmd(data):
+    assume(u'/*$' not in data)
+
     assert parse(data) == data
