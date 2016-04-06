ab6c59766e40f63fb9845c4534b6d5060eb88597
Peter Román <peterrom@gmail.com>
2016-04-06 19:54:32 +0200

    Add simple test runner to testfile.

--- makefile
+++ makefile
@@ -1,2 +1,6 @@
 4p: 4p.c makefile
 	c99 -g 4p.c -o 4p
+
+.PHONY: check
+check: 4p
+	./test4p.py
--- test4p.py
+++ test4p.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from subprocess import Popen, PIPE
 
 from hypothesis import given, assume
@@ -16,3 +17,29 @@ def test_no_cmd(data):
     assume(u'/*$' not in data)
 
     assert parse(data) == data
+
+
+if __name__ == '__main__':
+    import sys
+
+    def is_test(v):
+        try:
+            return v.func_name.startswith('test_')
+        except AttributeError:
+            return False
+
+    tests = (v for v in globals().copy().itervalues() if is_test(v))
+
+    for test in tests:
+        print test.func_name, '...',
+
+        try:
+            test()
+        except AssertionError:
+            print 'Fail'
+        except:
+            print 'Error'
+        else:
+            print 'Pass'
+        finally:
+            sys.stdout.flush()
